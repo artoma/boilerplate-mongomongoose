c553f33c6ca18ea4d68cbcf5fa069cd95d484662
artoma <artoma@gmail.com>
2023-04-12 12:15:10 +0300

    feat: find by id and remove

--- myApp.js
+++ myApp.js
@@ -78,7 +78,10 @@ const findAndUpdate = (personName, done) => {
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId, (err, person) => {
+    if(err) return console.log(err)
+    done(null, person)
+  })
 };
 
 const removeManyPeople = (done) => {
